207ad28e74ac0c91749ee14c78e0c4e433cde0c1
dislersd <dislersd@gmail.com>
2019-04-03 13:49:22 -0700

    set up get users route

--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,13 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Users = require('./users-model.js');
-const restricted = require('../auth/restricted-middleware.js');
+const Users = require("./users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'error getting users' })
+  }
 });
