12a3cb5a39850f22375f8ded095f7266a0afada6
dislersd <dislersd@gmail.com>
2019-04-03 18:41:39 -0700

    cleaned up code

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,23 +1,22 @@
-const jwt = require("jsonwebtoken");
-const secret = require('./secret.js')
+// STEP 5 - refactor restricted to use jwt 
 
+const jwt = require("jsonwebtoken");
+const secret = require("./secret.js");
 
 module.exports = (req, res, next) => {
-  // STEP 5 - refactor restricted
-  // const { username, password } = req.headers; - no longer need
   // STEP 5.1 - define token as authorization in headers
   const token = req.headers.authorization;
   if (token) {
     // STEP 5.2 verify with jwt library
     jwt.verify(token, secret.jwtSecret, (error, decodedToken) => {
-      if(error) {
-        res.status(401).json({ message: 'Invalid Creds' })
+      if (error) {
+        res.status(401).json({ message: "Invalid Creds" });
       } else {
         req.decodedJwt = decodedToken;
         next();
       }
-    })
+    });
   } else {
-    res.status(401).json({ message: 'No token provided' })
+    res.status(401).json({ message: "No token provided" });
   }
 };
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
+// libraries
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
+// routers
 const authRouter = require('./auth/auth-router.js');
 const usersRouter = require('./users/users-router.js');
-
+// server
 const server = express();
-
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
-
+//
 server.get('/', (req, res) => {
   res.send('Hello World')
 });
