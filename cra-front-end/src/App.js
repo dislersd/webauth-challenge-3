6a08a326aa7f3ada0c4f9b0301017d22e84d95ca
dislersd <dislersd@gmail.com>
2019-04-04 13:26:04 -0700

    added react router

--- cra-front-end/src/App.js
+++ cra-front-end/src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import styled from "styled-components";
+import { Route, NavLink } from "react-router-dom";
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 100px;
+`;
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        hello
-      </div>
-    );
+    return <AppWrapper>hello</AppWrapper>;
   }
 }
 
